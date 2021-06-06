8086f66a4d03771a0ac9e9cc28fac7bc73acdf70
Omar <omar0940@gmail.com>
2021-06-06 17:05:46 +0200

    React instagram

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,21 @@
 const Header = () => {
   return (
     <div className="header">
-      <span>Logo of Instagram</span>
-
-      <input type="text"></input>
-      <ul>
-        <li>Home</li>
-        <li>Inbox</li>
-        <li>Explore</li>
-        <li>Activity</li>
-        <li>user</li>
-      </ul>
+      <div className="logoNav">
+        <img src="./img/instagram-logo.png"></img>
+      </div>
+      <div className="search">
+        <input type="text" placeholder="Search"></input>
+      </div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Inbox</li>
+          <li>Explore</li>
+          <li>Activity</li>
+          <li>user</li>
+        </ul>
+      </nav>
     </div>
   );
 };
