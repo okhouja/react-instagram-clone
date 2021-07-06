1af33de8bfae3993546ccf32150015132babc4fd
Omar <omar0940@gmail.com>
2021-07-06 16:28:41 +0200

    design header & main

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,36 @@
 const Header = () => {
   return (
-    <div className="header">
-      <div className="logoNav">
-        <img src={`${process.env.PUBLIC_URL}/img/instagram-logo.png`}></img>
+    <div id="header__container">
+      <div className="header">
+        <div className="logoNav">
+          <img src={`${process.env.PUBLIC_URL}/img/instagram-logo.png`}></img>
+        </div>
+        <div className="search">
+          <input type="text" placeholder="Search"></input>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <img src={`${process.env.PUBLIC_URL}/img/icon/home.png`}></img>
+            </li>
+            <li>
+              <img src={`${process.env.PUBLIC_URL}/img/icon/inbox.png`}></img>
+            </li>
+            <li>
+              <img src={`${process.env.PUBLIC_URL}/img/icon/exploer.png`}></img>
+            </li>
+            <li>
+              {" "}
+              <img
+                src={`${process.env.PUBLIC_URL}/img/icon/activity.png`}
+              ></img>
+            </li>
+            <li>
+              <img src={`${process.env.PUBLIC_URL}/img/icon/user.png`}></img>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <div className="search">
-        <input type="text" placeholder="Search"></input>
-      </div>
-      <nav>
-        <ul>
-          <li>
-            <img src={`${process.env.PUBLIC_URL}/img/icon/home.png`}></img>
-          </li>
-          <li>
-            <img src={`${process.env.PUBLIC_URL}/img/icon/inbox.png`}></img>
-          </li>
-          <li>
-            <img src={`${process.env.PUBLIC_URL}/img/icon/exploer.png`}></img>
-          </li>
-          <li>
-            {" "}
-            <img src={`${process.env.PUBLIC_URL}/img/icon/activity.png`}></img>
-          </li>
-          <li>
-            <img src={`${process.env.PUBLIC_URL}/img/icon/user.png`}></img>
-          </li>
-        </ul>
-      </nav>
     </div>
   );
 };
