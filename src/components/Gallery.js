ab1ffeaafb0d011bb6117c37195f1dba305ed37f
Omar <omar0940@gmail.com>
2021-07-10 12:53:18 +0200

    parseFloat nubmer

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,9 +1,9 @@
 const Gallery = ({ posts }) => {
   const numFormatter = (num) => {
     if (num > 999 && num < 1000000) {
-      return (num / 1000).toFixed(1) + "K"; // convert to K for number from > 1000 < 1 million
+      return parseFloat((num / 1000).toFixed(1)) + "K"; // convert to K for number from > 1000 < 1 million
     } else if (num > 1000000) {
-      return (num / 1000000).toFixed(1) + "M"; // convert to M for number from > 1 million
+      return parseFloat((num / 1000000).toFixed(1)) + "M"; // convert to M for number from > 1 million
     } else if (num < 900) {
       return num; // if value < 1000, nothing to do
     }
@@ -162,7 +162,7 @@ const Gallery = ({ posts }) => {
                     className="commentonImage"
                     src={`${process.env.PUBLIC_URL}/img/comment.svg`}
                   ></img>
-                  {el.comments}
+                  {numFormatter(el.comments)}
                 </div>
               </div>
             </li>
